d29fb9f4fbc4f62efa3533aae3bfd25108378ebd
Avinash <avinash@wearedigitl.co.uk>
2025-06-23 16:26:27 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+
+<body>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+</body>
+
+</html>
